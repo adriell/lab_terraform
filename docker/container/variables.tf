fecf62c86baf7fcbec1b2f00dff26f692f6a64dc
Team Ghost <team.ghost@rivendel.com.br>
2019-03-05 09:43:27 -0300

    Added terraform module for docker

---
+++ docker/container/variables.tf
@@ -0,0 +1,19 @@
+# Variables the container
+variable "name" {
+    description = "Name of the container"
+}
+
+variable "int_port" {
+    description = "Ip address for int port"  
+}
+
+variable "ext_port" {
+    description = "Ip address for ext port"  
+}
+
+variable "image" {
+    description = "Name of image"  
+}
+
+
+
